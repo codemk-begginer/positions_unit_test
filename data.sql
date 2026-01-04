3577b341f6f4558071a2a837df32671a43571d49
codemk-begginer <metenousteve@gmail.com>
2026-01-04 06:39:17 +0100

    unit test with spring boot

---
+++ data.sql
@@ -0,0 +1,10 @@
+
+
+delete FROM customers;
+
+insert into customers (email) values ('metenousteve@gmail.com');
+insert into customers (email) values ('stevemetenou8@gmail.com');
+insert into customers (email) values ('mskgold8@gmail.com');
+insert into customers (email) values ('gmetenou7@gmail.com');
+insert into customers (email) values ('metenoup1@gmail.com');
+insert into customers (email) values ('mskgold11@outlook.fr');
